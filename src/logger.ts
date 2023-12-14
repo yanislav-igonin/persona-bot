c2861b9824436ca8abc07d25cb18be8469ce14f2
Yanislav Igonin <yanislavdabest@gmail.com>
2023-12-14 12:06:20 +0200

    add time log

--- src/logger.ts
+++ src/logger.ts
@@ -1,6 +1,19 @@
+const now = () => {
+  const date = new Date();
+  const [year, month, day, hour, minute, second] = [
+    date.getUTCFullYear(),
+    date.getUTCMonth() + 1,
+    date.getUTCDate(),
+    date.getUTCHours(),
+    date.getUTCMinutes(),
+    date.getUTCSeconds(),
+  ].map((value) => value.toString().padStart(2, '0'));
+  return `${year}-${month}-${day} ${hour}:${minute}:${second}`;
+};
+
 /* eslint-disable no-console */
-const info = (data: unknown) => console.log(data);
-const error = (data: unknown) => console.error(data);
+const info = (data: unknown) => console.log(now(), data);
+const error = (data: unknown) => console.error(now(), data);
 /* eslint-enable no-console */
 
 export const logger = {
